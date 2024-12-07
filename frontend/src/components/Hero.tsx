ee0a59154fe5ccead732e8df2b78f1a84ccb39e3
naz2510 <peenazshaikh2297@gmail.com>
2024-12-07 14:22:01 +0530

    first commit

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,10 @@
+import fc from "../assets/fc.png"
+const Hero = () => {
+  return(
+    <div>
+        <img src={fc} className="w-full max-h-[600px] object-cover"/>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ frontend/src/config/restaurant-options-config.ts
@@ -0,0 +1,30 @@
+export const cuisineList = [
+
+    "Rolls",
+    "Rice",
+    "Breakfast",
+    "Burgers",
+    "Cafe",
+    "Chinese",
+    "Desserts",
+    "Paratha",
+    "Greek",
+    "Healthy",
+    "Indian",
+    "Vada Pav",
+    "Ice Cream",
+    "Burger",
+    "Noodles",
+    "Organic",
+    "Pasta",
+    "Pizza",
+    "Salads",
+    "Cold drinks",
+    "Spanish",
+    "Wraps",
+    "Toasts",
+
+  ];
+
+
+
